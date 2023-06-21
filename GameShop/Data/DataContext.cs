05e2c750ec6c5f961e854531361e21b9d9a2bd58
Gut Jakub <w65539@student.wsiz.edu.pl>
2023-06-22 01:59:31 +0200

    Created Category Model and Controller Connected to an SQLite Database Created View for Category Controller

---
+++ GameShop/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using GameShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameShop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        /*
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+        */
+    }
+}
--- GameShop/Program.cs
+++ GameShop/Program.cs
@@ -1,7 +1,12 @@
+global using GameShop.Data;
+global using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DeafultConnection")));
 
 var app = builder.Build();
 
