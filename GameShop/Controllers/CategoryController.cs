22d4c7bc3d2e07cba21df13421517520cdba3819
Rogala Przemysław <w65566@student.wsiz.edu.pl>
2023-06-23 19:46:01 +0200

    Category view changes Server-side validation Edit and Delete buttons Update functionality

--- GameShop/Controllers/CategoryController.cs
+++ GameShop/Controllers/CategoryController.cs
@@ -19,5 +19,50 @@ namespace GameShop.Controllers
 		{
 			return View();
 		}
+		[HttpPost]
+		public IActionResult Create(Category objCategory)		
+		{
+		//	if (objCategory.Name.ToLower() == "validation test")
+		//	{
+		//		ModelState.AddModelError("Name", "The name cannot be validation test");
+		//	}
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(objCategory);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+			
+		}
+		public IActionResult Edit(int? id)
+		{
+			if(id==null || id == 0)
+			{
+				return NotFound();
+			}
+			Category categoryfromDb = _db.Categories.Find(id);
+			if(categoryfromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryfromDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category objCategory)
+		{
+			//	if (objCategory.Name.ToLower() == "validation test")
+			//	{
+			//		ModelState.AddModelError("Name", "The name cannot be validation test");
+			//	}
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(objCategory);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+
+		}
 	}
 }
--- GameShop/Models/Category.cs
+++ GameShop/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace GameShop.Models
 {
@@ -7,7 +8,10 @@ namespace GameShop.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
+        [DisplayName("Category")]
         public string Name { get; set; }
-        public int DisplayOrder { get; set; }
+		[Range(1, 100, ErrorMessage = "Display Order for category must be between 1 and 100")]
+		public int DisplayOrder { get; set; }
     }
 }
