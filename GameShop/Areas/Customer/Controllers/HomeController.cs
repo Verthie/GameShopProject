38abafeab9765a4657d6f31cef306283ad06a068
Rogala Przemysław <w65566@student.wsiz.edu.pl>
2023-06-24 22:44:43 +0200

    Home and Details for Customers

--- GameShop/Areas/Customer/Controllers/HomeController.cs
+++ GameShop/Areas/Customer/Controllers/HomeController.cs
@@ -1,23 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.DataAccess.Repository.IRepository;
 using Shop.Models;
 using System.Diagnostics;
 
 namespace GameShop.Areas.Customer.Controllers
 {
-	[Area("Customer")]
+    [Area("Customer")]
 
-	public class HomeController : Controller
+    public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+        public IActionResult Details(int id)
+        {
+            Product product = _unitOfWork.Product.Get(u=>u.Id==id,includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
